1c07665e424f163675c3c5a15b86ca770c2c9225
Guglielmo D'Anna <gug.danna@gmail.com>
2024-11-15 16:19:08 +0000

    added textarea, button, input native components

--- src/webviews/@preset/form.tsx
+++ src/webviews/@preset/form.tsx
@@ -1,12 +1,12 @@
 
-import { VSCodeButton } from "vscode-elements/button";
-import { VSCodeTextArea } from "vscode-elements/textarea";
 import { ANTHROPIC_MODELS, PermissionState, Preset, Provider } from "../../model";
 import { VSCodeLabel } from "vscode-elements/label";
-import { VSCodeTextfield } from "vscode-elements/text-field";
 import * as React from "react";
-import { Checkbox, CheckboxInput } from "components/checkbox";
+import { Checkbox } from "components/checkbox";
 import { Select, Option, SelectProps } from "components/select";
+import { Input } from "components/input";
+import { Textarea } from "components/textarea";
+import { Button } from "components/button";
 
 
 interface PresetFormProps extends React.DetailedHTMLProps<React.FormHTMLAttributes<HTMLFormElement>, HTMLFormElement> {
@@ -30,68 +30,68 @@ export function PresetForm(props: PresetFormProps) {
       </Select>
 
       <fieldset>
-        <VSCodeLabel htmlFor="api-key">
+        <legend aria-hidden>API</legend>
+        <label htmlFor="api-key">
           APIKey
-        </VSCodeLabel>
-        <VSCodeTextfield className="w-full" id="api-key" name="api-key" type="password" />
-        <Checkbox>
-          <CheckboxInput name="custom-base-URL" />
+          <Input className="w-full" id="api-key" name="api-key" type="password" />
+        </label>
+        <label>
+          <Checkbox name="custom-base-URL" />
           Use custom base URL
-        </Checkbox>
+        </label>
       </fieldset>
 
-
-
-      <VSCodeLabel>
+      <label>
         Model
-      </VSCodeLabel>
-      <Select className="w-full" defaultValue={initialData?.model || ANTHROPIC_MODELS[0]}>
-        {ANTHROPIC_MODELS.map((model) => (
-          <Option key={model} value={model}>{model}</Option>
-        ))}
-      </Select>
+        <Select className="w-full" defaultValue={initialData?.model || ANTHROPIC_MODELS[0]}>
+          {ANTHROPIC_MODELS.map((model) => (
+            <Option key={model} value={model}>{model}</Option>
+          ))}
+        </Select>
+      </label>
+
 
       <fieldset>
         <legend>Permissions</legend>
         <p>SōtaPR is smart, careful, and uses git to save changes - it works best with all permissions set to 'always'.</p>
-        <ul>
-          <li>
+        <ul className="grid grid-cols-[auto,_1fr]">
+          <li className="contents">
             <label htmlFor="code-editing">
               Code editing
             </label>
             <PermissionSelect className="w-full" id="write-code" name="permissions[code-editing]" />
           </li>
-          <li>
+          <li className="contents">
             <label htmlFor="permissions.terminal-commands">
               Run terminal commands
             </label>
             <PermissionSelect className="w-full" id="permissions.terminal-commands" name="permissions[terminal-commands]" />
           </li>
-          <li>
+          <li className="contents">
             <label htmlFor="permissions.list-files">
               List files
             </label>
             <PermissionSelect className="w-full" id="permissions.list-files" name="permissions[list-files]" />
           </li>
-          <li>
+          <li className="contents">
             <label htmlFor="permissions.search-file-content-with-regex">
               Search file content with regex
             </label>
             <PermissionSelect className="w-full" id="permissions.search-file-content-with-regex" name="permissions[search-file-content-with-regex]" />
           </li>
-          <li>
+          <li className="contents">
             <label htmlFor="permissions.open-files">
               Open files
             </label>
             <PermissionSelect className="w-full" id="permissions.oapen-files" name="permissions[open-files]" />
           </li>
-          <li>
+          <li className="contents">
             <label htmlFor="permissions.lsp-diagnostics">
               Use LSP diagnostics
             </label>
             <PermissionSelect className="w-full" id="permissions.lsp-diagnostics" name="permissions[lsp-diagnostics]" />
           </li>
-          <li>
+          <li className="contents">
             <label htmlFor="permissions.followup-questions">
               Ask followup questions
             </label>
@@ -100,17 +100,20 @@ export function PresetForm(props: PresetFormProps) {
         </ul>
       </fieldset>
 
-      <VSCodeLabel htmlFor="name">
+      <label htmlFor="name">
         Preset name
-      </VSCodeLabel>
-      <VSCodeTextfield className="w-full" id="name" name="name" />
 
-      <VSCodeLabel htmlFor="custom-instructions">
+        <Input className="w-full" id="name" name="name" />
+      </label>
+
+
+      <label htmlFor="custom-instructions">
         Custom instructions
-      </VSCodeLabel>
-      <VSCodeTextArea className="w-full" id='custom-instructions' name="custom-instructions" />
+        <Textarea className="w-full" id='custom-instructions' name="custom-instructions" />
+      </label>
 
-      <VSCodeButton type="submit">Send</VSCodeButton>
+
+      <Button type="submit">Send</Button>
     </form>
   )
 }
