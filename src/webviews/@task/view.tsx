c498b7621d36dc014da94dd6820b65f38ec22efb
Naresh <ghostwriternr@gmail.com>
2024-11-21 13:56:19 +0000

    Improve spacing between exchanges

--- src/webviews/@task/view.tsx
+++ src/webviews/@task/view.tsx
@@ -119,7 +119,7 @@ export function TaskView() {
         </div>
       </header>
       <div className="flex flex-grow flex-col gap-2 overflow-x-hidden overflow-y-scroll">
-        <section className="flex-grow p-2">
+        <section className="flex-grow px-3 py-2">
           {exchanges && (
             <ol>
               {exchanges.map((exchange) => (
