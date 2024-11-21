919f8143aa8310306472a90320bcf7b871bff233
Naresh <ghostwriternr@gmail.com>
2024-11-21 15:49:34 +0000

    Add a button to clear session

--- src/webviews/components/input/InputToolbar.tsx
+++ src/webviews/components/input/InputToolbar.tsx
@@ -6,15 +6,31 @@ interface InputToolbarProps {
 
 function InputToolbar(props: InputToolbarProps) {
   return (
-    <div id="input-toolbar" className="flex items-center justify-between">
+    <div id="input-toolbar" className="flex items-center justify-between text-xs">
       <div className="flex items-center justify-start gap-2 whitespace-nowrap">
         <div className="items-center gap-1 text-gray-400 transition-colors duration-200">
-          <div onClick={props.onAddContextItem} className="cursor-pointer px-1 py-0.5">
-            <span className="codicon codicon-plus h-4 w-4" />
+          <div
+            onClick={props.onAddContextItem}
+            className="flex cursor-pointer items-center rounded p-0.5 text-foreground hover:bg-panel-background"
+            role="button"
+            tabIndex={0}
+          >
+            <span className="codicon codicon-plus" />
           </div>
         </div>
       </div>
-      <div className="flex items-center gap-2 whitespace-nowrap text-gray-400">
+      <div className="flex items-center gap-0.5 whitespace-nowrap text-gray-400">
+        <div
+          className="flex cursor-pointer items-center justify-center rounded px-1.5 py-0.5 text-foreground hover:bg-panel-background disabled:cursor-wait"
+          onClick={(e) => {
+            props.onEnter?.();
+          }}
+          role="button"
+          tabIndex={0}
+        >
+          <span className="mr-[2px]">^L</span>
+          <span className="text-disabled-foreground">Clear</span>
+        </div>
         <div
           className="flex cursor-pointer items-center rounded px-1.5 py-0.5 text-foreground hover:bg-panel-background disabled:cursor-wait"
           onClick={(e) => {
@@ -24,7 +40,7 @@ function InputToolbar(props: InputToolbarProps) {
           tabIndex={0}
         >
           <span className="codicon codicon-newline mr-[2px]" />
-          <span className="select-none">Send</span>
+          <span className="text-disabled-foreground">Send</span>
         </div>
       </div>
     </div>
