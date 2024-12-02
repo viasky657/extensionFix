6ec7d711755bddd25b6cf10a07472654cc587e3f
Guglielmo D'Anna <gug.danna@gmail.com>
2024-12-02 16:51:26 +0000

    updated sidecar hash

--- src/utilities/setupSidecarBinary.ts
+++ src/utilities/setupSidecarBinary.ts
@@ -48,7 +48,7 @@ export function getSidecarBinaryURL() {
 // if the version we are looking at is okay, or we need to download a new binary
 // for now, lets keep it as it is and figure out a way to update the hash on
 // important updates
-export const SIDECAR_VERSION = '07ac26c88a019e4a7a41a1afff5f34195ba5bf42502369ef6fe1693144542cd2';
+export const SIDECAR_VERSION = 'fb2ceb56d738038773910f17fab529fe1bb73d0941b448a36480852648752883';
 
 async function checkCorrectVersionRunning(url: string): Promise<boolean> {
   try {
